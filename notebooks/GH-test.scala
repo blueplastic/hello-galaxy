de67814dfa7b8de15bea675470834bec707cb026
SameerF <sameer@blueplastic.com>
2016-03-14 16:27:08 -0700

    2nd save.

--- notebooks/GH-test.scala
+++ notebooks/GH-test.scala
@@ -1,1 +1,17 @@
-// Databricks notebook source exported at Mon, 14 Mar 2016 23:25:40 UTC
+// Databricks notebook source exported at Mon, 14 Mar 2016 23:27:09 UTC
+1+1
+
+// COMMAND ----------
+
+val x = 20
+
+// COMMAND ----------
+
+x + 3
+
+// COMMAND ----------
+
+// MAGIC %md Here is some markdown to end the notebook!
+
+// COMMAND ----------
+
